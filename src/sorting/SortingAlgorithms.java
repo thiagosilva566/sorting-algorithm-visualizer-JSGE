2a791987cf46fa6a8d5afbf957236443fa22fbbd
Thiago Silva <thiago.oliveira050506@gmail.com>
2026-03-26 16:10:48 -0300

    adding method names

---
+++ src/sorting/SortingAlgorithms.java
@@ -0,0 +1,28 @@
+package sorting;
+
+public class SortingAlgorithms {
+
+    public static void selectionSort() {
+
+    }
+
+    public static void insertionSort() {
+
+    }
+
+    public static void ShellSort() {
+
+    }
+
+    public static void MergeSort() {
+
+    }
+
+    public static void bucketSort() {
+
+    }
+
+    public static void quickSort() {
+
+    }
+}
