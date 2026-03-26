19848a2265df63b2760ee17cdd78ce7d8e4c4e64
Thiago Silva <thiago.oliveira050506@gmail.com>
2026-03-26 16:21:44 -0300

    implementation of selectionSort

--- src/sorting/SortingAlgorithms.java
+++ src/sorting/SortingAlgorithms.java
@@ -1,8 +1,20 @@
 package sorting;
 
+import static utils.ArrayUtils.swap;
+
 public class SortingAlgorithms {
 
-    public static void selectionSort() {
+    public static void selectionSort( int[] array ) {
+        int n = array.length;
+        for ( int i = 0; i < n; i++ ) {
+            int min = i;
+            for ( int j = i + 1; j < n; j++ ) {
+                if ( array[j] < array[min] ) {
+                    min = j;
+                }
+            }
+            swap( array, i, min );
+        }
 
     }
 
