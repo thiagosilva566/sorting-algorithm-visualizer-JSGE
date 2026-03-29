91088f2d8be2af2981698a4e4e52469cbca1e9d0
thigas <thiago.oliveira050506@gmail.com>
2026-03-29 17:30:15 -0300

    fixed swap method

--- src/utils/ArrayUtils.java
+++ src/utils/ArrayUtils.java
@@ -7,6 +7,7 @@ public class ArrayUtils {
     public static void swap(int[] array, int i, int j ) {
         int temp = array[i];
         array[i] = array[j];
+        array[j] = temp;
     }
 
     public static int[] copy( int[] array ) {
