82dac44e0b7a4b240df1fbd54ff4c1774c502587
thigas <thiago.oliveira050506@gmail.com>
2026-03-29 00:02:31 -0300

    changes in the swap method to return information about the swap

--- src/utils/ArrayUtils.java
+++ src/utils/ArrayUtils.java
@@ -1,10 +1,18 @@
 package utils;
 
+import sorting.SortingInformation;
+import sorting.TypeComparison;
+
 public class ArrayUtils {
 
-    public static void swap( int[] array, int i, int j ) {
+    public static SortingInformation swap(int[] array, int i, int j ) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+        return new SortingInformation(
+                i,
+                j,
+                TypeComparison.SWAP
+        );
     }
 }
